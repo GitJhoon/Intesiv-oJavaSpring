31f30ff68d348cb56a51318e07979c379370fd68
Jonathan <Jonathan22office@gmail.com>
2024-11-15 15:30:24 -0300

    Adicionando as classes Belonging e BelongingPK

---
+++ src/main/java/com/example/IntensivaoJavaSpring/entities/BelongigPK.java
@@ -0,0 +1,8 @@
+package com.example.IntensivaoJavaSpring.entities;
+
+import java.util.List;
+
+public class BelongigPK {
+    private Game game;
+    private List<Game> gameList;
+}
---
+++ src/main/java/com/example/IntensivaoJavaSpring/entities/Belonging.java
@@ -0,0 +1,10 @@
+package com.example.IntensivaoJavaSpring.entities;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+import java.util.List;
+
+public class Belonging {
+    private BelongigPK belongigPK;
+    private Integer position;
+}
